8c5a3ccfc27d7eb924ce937656e4bad0170480e2
wtn4105@corp.netease.com <wtn4105@corp.netease.com>
2017-10-03 23:52:30 +0800

    init with bottom navigation view

---
+++ app/src/main/java/holy/shit/instragram/HomeActivity.java
@@ -0,0 +1,18 @@
+package holy.shit.instragram;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+}
