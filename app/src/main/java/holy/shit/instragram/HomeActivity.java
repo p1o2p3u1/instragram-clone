fceaa746e5c814e5d85485420219e0f10ffc2dab
wtn4105@corp.netease.com <wtn4105@corp.netease.com>
2017-10-04 16:43:05 +0800

    bottom navigation view ex

--- app/src/main/java/holy/shit/instragram/HomeActivity.java
+++ app/src/main/java/holy/shit/instragram/HomeActivity.java
@@ -7,12 +7,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
 public class HomeActivity extends AppCompatActivity {
+
+    private static final String TAG = "HomeActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        renderView();
+    }
+
+    private void renderView(){
         setContentView(R.layout.activity_home);
+        BottomNavigationViewEx navigation = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
+        navigation.enableAnimation(false);
+        navigation.enableShiftingMode(false);
+        navigation.enableItemShiftingMode(false);
+        navigation.setTextVisibility(false);
     }
 
 }
